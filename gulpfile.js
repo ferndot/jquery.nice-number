8ca89ad1ed0b65ae7990d87fb2acb92c791a2bee
frankie-tech <26461046+frankie-tech@users.noreply.github.com>
2020-08-14 11:37:21 -0400

    updates the gulp file to match gulp4 api
    moves away from one singular pipeline task
    into multiple tasks through one gulp parallel

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,75 @@ var uglifycss = require('gulp-uglifycss');
 var pump = require('pump');
 
 gulp.task('default', function (cb) {
-  pump(
-    [
-      gulp.src('src/**/*.js'),
-      uglify(),
-      gulp.dest('dist'),
+	pump(
+		[
+			gulp.src('src/**/*.js'),
+			uglify(),
+			gulp.dest('dist'),
 
-      gulp.src('src/**/*.css'),
-      uglifycss({
-        "maxLineLen": 80,
-        "uglyComments": true
-      }),
-      rename(function (path) {
-        path.basename += ".min";
-      }),
-      gulp.dest('dist'),
+			gulp.src('src/**/*.css'),
+			uglifycss({
+				maxLineLen: 80,
+				uglyComments: true,
+			}),
+			rename(function (path) {
+				path.basename += '.min';
+			}),
+			gulp.dest('dist'),
 
-      gulp.src('src/**/*'),
-      gulp.dest('dist'),
-    ],
-    cb
-  );
+			gulp.src('src/**/*'),
+			gulp.dest('dist'),
+		],
+		cb,
+	);
 });
+
+var { src, dest, series, parallel } = require('gulp');
+var paths = {
+	src: 'src/**/*',
+	js: 'src/**/*.js',
+	css: 'src/**/*.css',
+	dist: './dist/',
+};
+
+function addMin(path) {
+	path.basename += '.min';
+}
+
+function copy() {
+	return src(paths.src).pipe(dest(paths.dist));
+}
+
+copy.description =
+	'Copies src folder directly to the dist folder, no mutations';
+
+function css() {
+	return src(paths.css)
+		.pipe(
+			uglifycss({
+				maxLineLen: 80,
+				uglyComments: true,
+			}),
+		)
+		.pipe(rename(addMin))
+		.pipe(dest(paths.dist));
+}
+
+css.description =
+	'Mutates the css files, including minifying, and adding ".min" to the basename';
+
+function js() {
+	return src(paths.js)
+		.pipe(uglify())
+		.pipe(rename(addMin))
+		.pipe(dest(paths.dist));
+}
+
+js.description =
+	'Mutates the js files, including minifying, and adding ".min" to the basename';
+
+exports.copy = copy;
+exports.css = css;
+exports.js = js;
+
+exports.build = parallel(css, js, copy);
