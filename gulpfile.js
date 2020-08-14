5df2861b630498d3a4f281caaf8b6e6bcb9e5aa7
frankie-tech <26461046+frankie-tech@users.noreply.github.com>
2020-08-14 13:06:59 -0400

    cleaned out old tasks and set new default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,6 @@
-var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
-var pump = require('pump');
-
-gulp.task('default', function (cb) {
-	pump(
-		[
-			gulp.src('src/**/*.js'),
-			uglify(),
-			gulp.dest('dist'),
-
-			gulp.src('src/**/*.css'),
-			uglifycss({
-				maxLineLen: 80,
-				uglyComments: true,
-			}),
-			rename(function (path) {
-				path.basename += '.min';
-			}),
-			gulp.dest('dist'),
-
-			gulp.src('src/**/*'),
-			gulp.dest('dist'),
-		],
-		cb,
-	);
-});
 
 var { src, dest, series, parallel, watch } = require('gulp');
 var paths = {
@@ -86,5 +60,5 @@ exports.copy = copy;
 exports.css = css;
 exports.js = js;
 
-exports.build = parallel(copy, css, js);
-exports.dev = series(exports.build, source);
+exports.default = parallel(copy, css, js);
+exports.dev = series(exports.default, source);
